ab8d3e9942ee0fce20613fbd664365c0553d8d0f
ronald li <ronailhk@gmail.com>
2022-09-03 17:19:45 +0800

    Adds full_game helper file and corresponding error

---
+++ x/checkers/types/full_game.go
@@ -0,0 +1,37 @@
+package types
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/alice/checkers/x/checkers/rules"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+func (storedGame *StoredGame) GetCreatorAddress() (creator sdk.AccAddress, err error) {
+	creator, errCreator := sdk.AccAddressFromBech32(storedGame.Creator)
+	return creator, sdkerrors.Wrapf(errCreator, ErrInvalidCreator.Error(), storedGame.Creator)
+}
+
+func (storedGame *StoredGame) GetRedAddress() (red sdk.AccAddress, err error) {
+	red, errRed := sdk.AccAddressFromBech32(storedGame.Red)
+	return red, sdkerrors.Wrapf(errRed, ErrInvalidRed.Error(), storedGame.Red)
+}
+
+func (storedGame *StoredGame) GetBlackAddress() (red sdk.AccAddress, err error) {
+	black, errBlack := sdk.AccAddressFromBech32(storedGame.Black)
+	return black, sdkerrors.Wrapf(errBlack, ErrInvalidBlack.Error(), storedGame.Red)
+}
+
+func (storedGame *StoredGame) ParseGame() (game *rules.Game, err error) {
+	game, errGame := rules.Parse(storedGame.Game)
+	if errGame != nil {
+		return nil, sdkerrors.Wrapf(errGame, ErrGameNotParseable.Error())
+	}
+	game.Turn = rules.StringPieces[storedGame.Turn].Player
+	if game.Turn.Color == "" {
+		return nil, sdkerrors.Wrapf(errors.New(fmt.Sprintf("Turn: %s", storedGame.Turn)), ErrGameNotParseable.Error())
+	}
+	return game, nil
+}
