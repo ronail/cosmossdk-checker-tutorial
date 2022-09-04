58aaa9524501e4c96a137cf2baca8c4f896184a4
ronald li <ronailhk@gmail.com>
2022-09-04 17:26:10 +0800

    Adds CreateGame message handling and Unit Test

--- x/checkers/keeper/msg_server_create_game.go
+++ x/checkers/keeper/msg_server_create_game.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -11,7 +13,33 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
-	_ = ctx
+	nextGame, found := k.Keeper.GetNextGame(ctx)
+	if !found {
+		panic("NextGame not found")
+	}
+	newIndex := strconv.FormatUint(nextGame.IdValue, 10)
 
-	return &types.MsgCreateGameResponse{}, nil
+	newGame := rules.New()
+	storedGame := types.StoredGame {
+		Creator: msg.Creator,
+    Index:   newIndex,
+    Game:    newGame.String(),
+    Turn:    rules.PieceStrings[newGame.Turn],
+    Red:     msg.Red,
+    Black:   msg.Black,
+	}
+
+	err := storedGame.Validate()
+	if err != nil {
+	    return nil, err
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	nextGame.IdValue++
+	k.Keeper.SetNextGame(ctx, nextGame)
+
+	return &types.MsgCreateGameResponse{
+		IdValue: newIndex,
+	}, nil
 }
