051f4a90006a9238d90cc21562cf72c16c98e45e
ronald li <ronailhk@gmail.com>
2022-09-06 23:02:51 +0800

    Adds Unit Test TestPlayMoveEmitted

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -42,3 +42,30 @@ func TestPlayMove(t *testing.T) {
         Winner:    "*",
     }, *playMoveResponse)
 }
+
+func TestPlayMoveEmitted(t *testing.T) {
+    msgServer, _, context := setupMsgServerWithOneGameForPlayMove(t)
+    msgServer.PlayMove(context, &types.MsgPlayMove{
+        Creator: carol,
+        IdValue: "1",
+        FromX:   1,
+        FromY:   2,
+        ToX:     2,
+        ToY:     3,
+    })
+    ctx := sdk.UnwrapSDKContext(context)
+    require.NotNil(t, ctx)
+    events := sdk.StringifyEvents(ctx.EventManager().ABCIEvents())
+    require.Len(t, events, 1)
+    event := events[0]
+    require.Equal(t, event.Type, "message")
+    require.EqualValues(t, []sdk.Attribute{
+        {Key: "module", Value: "checkers"},
+        {Key: "action", Value: "MovePlayed"},
+        {Key: "Creator", Value: carol},
+        {Key: "IdValue", Value: "1"},
+        {Key: "CapturedX", Value: "-1"},
+        {Key: "CapturedY", Value: "-1"},
+        {Key: "Winner", Value: "*"},
+    }, event.Attributes[6:])
+}
