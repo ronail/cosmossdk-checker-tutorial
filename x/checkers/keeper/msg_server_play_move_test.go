671830e19a32ddfce5586f94d5bcaa685f88a98f
ronald li <ronailhk@gmail.com>
2022-09-06 22:57:50 +0800

    fixes unit test

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -11,13 +11,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	alice = "cosmos1jmjfq0tplp9tmx4v9uemw72y4d2wa5nr3xn9d3"
-	bob   = "cosmos1xyxs3skf3f4jfqeuv89yyaqvjc6lffavxqhc8g"
-	carol = "cosmos1e0w5t53nrq7p66fye6c8p0ynyhf6y24l4yuxd7"
-)
-
-
 func setupMsgServerWithOneGameForPlayMove(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
     k, ctx := setupKeeper(t)
     checkers.InitGenesis(ctx, *k, *types.DefaultGenesis())
